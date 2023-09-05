215b7e2b4c23aca9dfeb29a816566a3eb8738c88
Dante01Rus <eshev-d@yandex.ru>
2023-09-05 16:22:14 +0300

    правки

--- source/js/modules/menu/menu.js
+++ source/js/modules/menu/menu.js
@@ -1,18 +1,31 @@
 const nav = document.querySelector('.nav__list');
 const navToggle = document.querySelector('.header__burger-btn');
 const header = document.querySelector('.header');
+const pageMain = document.querySelector('.main');
+const pageWrapper = document.querySelector('.wrapper');
 
 nav.classList.remove('nav__list--nojs');
 header.classList.remove('header--nojs');
 
-navToggle.addEventListener('click', function () {
+function menuBtnOnclick() {
   if (nav.classList.contains('nav__list--closed')) {
     header.classList.add('header__menu-opened');
+    pageMain.classList.add('main__menu-opened');
+    pageWrapper.classList.add('modal-open');
     nav.classList.remove('nav__list--closed');
     nav.classList.add('nav__list--opened');
   } else {
     header.classList.remove('header__menu-opened');
+    pageMain.classList.remove('main__menu-opened');
+    pageWrapper.classList.remove('modal-open');
     nav.classList.add('nav__list--closed');
     nav.classList.remove('nav__list--opened');
+  }
+}
+
+navToggle.addEventListener('click', menuBtnOnclick);
+nav.addEventListener('click', (evt) => {
+  if (evt.target.classList.contains('nav__link')) {
+    menuBtnOnclick();
   }
 });
